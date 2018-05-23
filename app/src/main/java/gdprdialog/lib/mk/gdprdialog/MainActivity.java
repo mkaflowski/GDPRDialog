e2780c275d789a44c9f24b19235e6b42f329cbd2
mateuszkaflowski <mkaflowski@gmail.com>
2018-05-23 15:19:34 +0200

    initi

---
+++ app/src/main/java/gdprdialog/lib/mk/gdprdialog/MainActivity.java
@@ -0,0 +1,13 @@
+package gdprdialog.lib.mk.gdprdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
